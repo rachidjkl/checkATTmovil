33d9b315cffc4d890ea2bc8e6aa21f28ecc8ad6f
rachid <rghenema2122@politecnics.barcelona>
2023-03-07 13:46:51 +0100

    a

--- app/src/main/java/com/example/menulateral/Login.kt
+++ app/src/main/java/com/example/menulateral/Login.kt
@@ -14,6 +14,7 @@ class Login : AppCompatActivity() {
 
 
 
+
         btnLogin.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
