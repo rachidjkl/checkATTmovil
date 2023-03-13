65beabf50c914081fe39fbb28e7a1f2f453db091
MarioLeiva <mleiva@alumno.santoangel.org>
2023-03-13 13:37:21 +0100

    rhfg

--- app/src/main/java/com/example/menulateral/ui/faltasJustificadas/FaltasJustificadasFragment.kt
+++ app/src/main/java/com/example/menulateral/ui/faltasJustificadas/FaltasJustificadasFragment.kt
@@ -1,12 +1,17 @@
 package com.example.menulateral.ui.faltasJustificadas
 
+import android.graphics.*
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.menulateral.R
 import com.example.menulateral.databinding.FragmentFaltasJustificadasBinding
 
 class FaltasJustificadasFragment : Fragment() {
@@ -26,14 +31,36 @@ class FaltasJustificadasFragment : Fragment() {
             ViewModelProvider(this).get(FaltasJustificadasViewModel::class.java)
 
         _binding = FragmentFaltasJustificadasBinding.inflate(inflater, container, false)
+
+        val modulos = resources.getStringArray(R.array.spinner_options)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.dropdown_item, modulos)
+        binding.autoCompleteTextView6.setAdapter(arrayAdapter)
+
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        faltasJustificadasViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.alumno_ideal) // Cargar imagen desde drawable
+        val diameter = 200 // Diámetro del círculo
+
+        val scaledBitmap = Bitmap.createScaledBitmap(bitmap, diameter, diameter, true) // Escalar imagen al tamaño del círculo
+
+        val circleBitmap = Bitmap.createBitmap(diameter, diameter, Bitmap.Config.ARGB_8888) // Crear nuevo bitmap para el círculo
+        val canvas = Canvas(circleBitmap) // Dibujar en el nuevo bitmap
+        val paint = Paint()
+        paint.isAntiAlias = true
+        paint.color = Color.WHITE
+        canvas.drawCircle(diameter / 2f, diameter / 2f, diameter / 2f, paint) // Dibujar círculo blanco
+        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+        canvas.drawBitmap(scaledBitmap, 0f, 0f, paint) // Dibujar imagen en el círculo
+
+        binding.imageView.setImageBitmap(circleBitmap) // Mostrar el bitmap del círculo en el ImageView
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
