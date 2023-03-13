067667eeba090b92512286306349682884487750
MarioLeiva <mleiva@alumno.santoangel.org>
2023-03-13 11:08:24 +0100

    sdfg

--- app/src/main/java/com/example/menulateral/ui/justificarFalta/JustificarFaltaFragment.kt
+++ app/src/main/java/com/example/menulateral/ui/justificarFalta/JustificarFaltaFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -27,6 +28,28 @@ class JustificarFaltaFragment : Fragment() {
 
         _binding = FragmentJustificarFaltaBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        val stringList = listOf(
+            "Lorem ipsum dolor sit amet",
+            "Consectetur adipiscing elit",
+            "Sed do eiusmod tempor incididunt",
+            "Ut labore et dolore magna aliqua",
+            "Ut enim ad minim veniam",
+            "Quis nostrud exercitation ullamco",
+            "Laboris nisi ut aliquip ex ea commodo consequat",
+            "Duis aute irure dolor in reprehenderit in voluptate velit",
+            "Esse cillum dolore eu fugiat nulla pariatur",
+            "Excepteur sint occaecat cupidatat non proident",
+            "Sunt in culpa qui officia deserunt mollit anim id est laborum",
+            "Nemo enim ipsam voluptatem quia voluptas sit aspernatur",
+            "Aut odit aut fugit",
+            "Sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt",
+            "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet",
+            "Consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt"
+        )
+
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, stringList)
+        binding.recyclerView2.adapter = adapter
 
         val textView: TextView = binding.textReason
         return root
