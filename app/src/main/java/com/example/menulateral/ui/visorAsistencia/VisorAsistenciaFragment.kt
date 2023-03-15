681986b60cb8eddfe1f69b5e1e19b536cac86f74
rachid <rghenema2122@politecnics.barcelona>
2023-03-15 12:37:52 +0100

    ff

--- app/src/main/java/com/example/menulateral/ui/visorAsistencia/VisorAsistenciaFragment.kt
+++ app/src/main/java/com/example/menulateral/ui/visorAsistencia/VisorAsistenciaFragment.kt
@@ -2,7 +2,9 @@ package com.example.menulateral.ui.visorAsistencia
 
 import android.animation.Animator
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +13,7 @@ import android.view.animation.DecelerateInterpolator
 import android.view.animation.LinearInterpolator
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.menulateral.databinding.FragmentVisorAsistenciaBinding
@@ -31,14 +34,26 @@ class VisorAsistenciaFragment : Fragment() {
         _binding = FragmentVisorAsistenciaBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        //------------------------------------------------------PreogressBar-----------------------------------------------------------//
-        binding.progressBar.clearAnimation();
-        val currentProgres = 60000
-        binding.progressBar.max = 10000;
-        ObjectAnimator.ofInt( binding.progressBar, "progress", currentProgres)
-            .setDuration(15000)
-            .start()
-        //-----------------------------------------------------------------------------------------------------------------------------//
+        binding.progressBar.max = 100;
+        binding.progressBar.setProgress(0)
+        var cont = 0;
+        val timer = object: CountDownTimer(1500, 15) {
+            @SuppressLint("SetTextI18n")
+            override fun onTick(millisUntilFinished: Long) {
+                if (cont < 50){
+                    var aux = binding.textHome.text.toString()
+                    binding.textHome.text = (aux.toInt() + 1).toString()
+                    binding.progressBar.setProgress(binding.progressBar.progress +1)
+                    cont ++
+                }
+            }
+            override fun onFinish() {
+                binding.textHome.text = "50"
+                binding.progressBar.setProgress(50)
+
+            }
+        }
+        timer.start()
         return root
     }
 
