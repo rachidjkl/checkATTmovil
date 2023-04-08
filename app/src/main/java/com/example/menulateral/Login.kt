7ead9e40c16fbfd1e1f7888bddbdb521dfd08974
JoelCano1 <jmarcosc2122@politecnics.barcelona>
2023-04-08 11:18:28 +0200

    last

--- app/src/main/java/com/example/menulateral/Login.kt
+++ app/src/main/java/com/example/menulateral/Login.kt
@@ -13,6 +13,7 @@ import androidx.core.content.ContextCompat.startActivity
 import androidx.core.util.PatternsCompat
 import com.example.menulateral.ApiAcces.ApiGetUserCep
 import com.example.menulateral.ApiAcces.RetrofitClient
+import com.example.menulateral.DataModel.UserCep
 import com.example.menulateral.databinding.ActivityLoginBinding
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -22,44 +23,47 @@ class Login : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private lateinit var UserCep: List<String>
     private lateinit var lstUserCep: ListView
+    private lateinit var userLogin: UserCep
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        userLogin = UserCep(40001, "jl@gmail.com", "1234",1);
 
-        lstUserCep = binding.listUsers
-        val userCepApi = RetrofitClient.getInstance().create(ApiGetUserCep::class.java)
+//        lstUserCep = binding.listUsers
+//        val userCepApi = RetrofitClient.getInstance().create(ApiGetUserCep::class.java)
+//
+//        GlobalScope.launch() {
+//            val call = userCepApi.getUsuarioCep()
+//            val response = call.execute()
+//            val localUserCep = response.body()
+//            if (localUserCep != null) {
+//
+//                UserCep = localUserCep
+//                runOnUiThread() {
+//                    val adapter = ArrayAdapter(this@Login, android.R.layout.simple_list_item_1, UserCep)
+//                    lstUserCep.setAdapter(adapter)
+//                }
+//
+//            } else {
+//                Toast.makeText(this@Login, "Error al consultar la API", Toast.LENGTH_SHORT).show()
+//            }
+//        }
 
-        GlobalScope.launch() {
-            val call = userCepApi.getUsuarioCep()
-            val response = call.execute()
-            val localUserCep = response.body()
-            if (localUserCep != null) {
 
-                UserCep = localUserCep
-                runOnUiThread() {
-                    val adapter = ArrayAdapter(this@Login, android.R.layout.simple_list_item_1, UserCep)
-                    lstUserCep.setAdapter(adapter)
-                }
-
-            } else {
-                Toast.makeText(this@Login, "Error al consultar la API", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        binding.btnLogin2.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
 
         binding.btnLogin.setOnClickListener {
             if (validar()) {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
+                if (userLogin.tipoUser == 1){
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }else{
+                    // intent a alumno
+                }
+
             }
 
         }
@@ -77,41 +81,40 @@ class Login : AppCompatActivity() {
 
 
     private fun validateEmail(): Boolean {
-        var next = true;
+        var next = false;
         val emailTxt = binding.email.editText?.text.toString()
         if (emailTxt.isEmpty()) {
             binding.email.error = "Filed can not be empty"
-            next = false;
         } else if (!PatternsCompat.EMAIL_ADDRESS.matcher(emailTxt).matches()) {
             binding.email.error = "Please enter a valid email address"
-            next = false;
-        } else {
-            binding.email.error = null
+        } else if(emailTxt == userLogin.correoCep) {
+            next = true;
+        }else{
+            binding.email.error = "Wrong email"
         }
         return next;
     }
 
 
     private fun validatePassword(): Boolean {
-        var next = true;
+        var next = false;
         val passwordTxt = binding.password.editText?.text.toString()
-        val passRegex = Pattern.compile(
-            "^" +
-                    "(?=.*[0-9])" +
-                    "(?=.*[a-z])" +
-                    "(?=.*[A-Z])" +
-                    ".{8,}" +
-                    "$"
-        )
+//        val passRegex = Pattern.compile(
+//            "^" +
+//                    "(?=.*[0-9])" +
+//                    "(?=.*[a-z])" +
+//                    "(?=.*[A-Z])" +
+//                    ".{8,}" +
+//                    "$"
+//        )
         if (passwordTxt.isEmpty()) {
             binding.password.error = "Filed can not be empty"
-            next = false;
-        } else if (!passRegex.matcher(passwordTxt).matches()) {
-            binding.password.error = "Please enter a valid password address"
-            next = false;
-        } else {
-            binding.password.error = null
-
+//        } else if (!passRegex.matcher(passwordTxt).matches()) {
+//            binding.password.error = "Please enter a valid password address"
+        } else if(passwordTxt == userLogin.contraUserCep) {
+            next = true;
+        }else{
+            binding.password.error = "Wrong password"
         }
         return next;
     }
