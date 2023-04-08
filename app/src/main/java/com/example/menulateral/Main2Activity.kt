64156f98e44b4311d61b7897547c84a1458caa2f
rachidjkl <rghenema2122@politecnics.barcelona>
2023-04-08 21:29:46 +0200

    ddd

--- app/src/main/java/com/example/menulateral/Main2Activity.kt
+++ app/src/main/java/com/example/menulateral/Main2Activity.kt
@@ -26,10 +26,7 @@ class Main2Activity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain2.toolbar)
 
-        binding.appBarMain2.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main2)
