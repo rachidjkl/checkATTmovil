8b73c827fecba6978e6fc4648998e906894ae949
MarcAlza <malzamoral2122@politecnics.barcelona>
2023-04-17 13:40:03 +0200

    definitivooo plus max 100% real

--- app/src/main/java/com/example/menulateral/ui/verfaltas/VerFaltasFragment.kt
+++ app/src/main/java/com/example/menulateral/ui/verfaltas/VerFaltasFragment.kt
@@ -1,54 +1,69 @@
 package com.example.menulateral.ui.verfaltas
 
+import android.os.Build
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.menulateral.ApiAcces.ApiGets
+import com.example.menulateral.ApiAcces.RetrofitClient
+import com.example.menulateral.DataModel.FaltaToShow
 import com.example.menulateral.DataModel.Faltas
+import com.example.menulateral.DataModel.FaltasPorFecha
 import com.example.menulateral.DataModel.Uf
+import com.example.menulateral.Login
+import com.example.menulateral.databinding.FragmentJustificarFaltaBinding
 import com.example.menulateral.databinding.FragmentVerFaltasBinding
+import com.example.menulateral.ui.visorAsistencia.justificarFaltaAdapter
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import java.time.LocalDate
 
-val faltasList = mutableListOf<Faltas>(
-    Faltas(1, 101, 1, "Medico"),
-    Faltas(2, 102, 1, "Trabajo"),
-    Faltas(3, 103, 1, "Trabajo"),
-    Faltas(4, 101, 2, "Examen onducir"),
-    Faltas(5, 102, 2, "Bar"),
-    Faltas(6, 103, 2, "Sida")
-)
-
-val ufList = mutableListOf<Uf>(
-    Uf(1, 101, 123, "UF1 - Introducción a la programación", "08", "09"),
-    Uf(2, 102, 124, "UF2 - Programación orientada a objetos", "09", "10"),
-    Uf(3, 103, 125, "UF3 - Estructuras de datos y algoritmos", "10", "11"),
-    Uf(4, 101, 126, "UF4 - Bases de datos y SQL", "11", "12"),
-    Uf(5, 102, 127, "UF5 - Desarrollo web con JavaScript", "12", "13"),
-    Uf(6, 103, 128, "UF6 - Desarrollo móvil con Kotlin", "13", "14")
-)
 
 class VerFaltasFragment : Fragment() {
 
 
     private var _binding: FragmentVerFaltasBinding? = null
 
+    private var faltasToShowListTotales: List<FaltaToShow>? = null
+
+    // LA CORRUTINA SE HA DE LLAMAR DESDE OTRA CORRUTINA
+    init {
+        main()
+    }
+    fun main() = runBlocking {
+        faltasToShowListTotales = globalFun()
+    }
+
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        var faltasfecha = agruparFaltasPorFecha(faltasToShowListTotales)
+
         val verFaltasViewModel =
             ViewModelProvider(this).get(VerFaltasViewModel::class.java)
 
+
+
         _binding = FragmentVerFaltasBinding.inflate(inflater, container, false)
-
-
         val root: View = binding.root
+
+        val adapter = verFaltasAdapter(faltasfecha)
+        binding.RecyclerViewVerFaltas.hasFixedSize()
+        binding.RecyclerViewVerFaltas.layoutManager = LinearLayoutManager(this.context)
+        binding.RecyclerViewVerFaltas.adapter = adapter
 
 
         return root
@@ -58,6 +73,42 @@ class VerFaltasFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun agruparFaltasPorFecha(faltasToShowListTotales: List<FaltaToShow>?): List<FaltasPorFecha> {
+        val faltasPorFecha = mutableMapOf<LocalDate, MutableList<FaltaToShow>>()
+
+        if (faltasToShowListTotales != null) {
+
+            for (falta in faltasToShowListTotales) {
+                val fecha = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    LocalDate.parse(falta.id_datetime.substringBefore("T"))
+                } else {
+                    val dateParts = falta.id_datetime.substringBefore("T").split("-")
+                    LocalDate.of(dateParts[0].toInt(), dateParts[1].toInt(), dateParts[2].toInt())
+                }
+
+                if (fecha in faltasPorFecha) {
+                    faltasPorFecha[fecha]?.add(falta)
+                } else {
+                    faltasPorFecha[fecha] = mutableListOf(falta)
+                }
+            }
+        }
+        return faltasPorFecha.entries.map { FaltasPorFecha(it.key, it.value) }
+    }
+
+
+
+    private suspend fun globalFun(): List<FaltaToShow>? {
+
+        val userCepApi = RetrofitClient.getInstance().create(ApiGets::class.java)
+
+        return GlobalScope.async {
+            val call = userCepApi.getFaltasToShowTotales(Login.alumno.idAlumno)
+            val response = call.execute()
+            response.body()
+        }.await()
     }
 
 
--- app/src/main/java/com/example/menulateral/ui/verfaltas/verFaltasAdapter.kt
+++ app/src/main/java/com/example/menulateral/ui/verfaltas/verFaltasAdapter.kt
@@ -1,4 +1,96 @@
 package com.example.menulateral.ui.verfaltas
 
-class verFaltasAdapter {
+import android.transition.ChangeBounds
+import android.transition.TransitionManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.menulateral.DataModel.FaltasPorFecha
+import com.example.menulateral.R
+import com.example.menulateral.ui.justificarFalta.UFCheckBoxAdapter
+import com.example.menulateral.ui.visorAsistencia.justificarFaltaAdapter
+
+class verFaltasAdapter ( private val faltasfecha : List<FaltasPorFecha>):
+    RecyclerView.Adapter<verFaltasAdapter.verFaltasHolder>(), View.OnClickListener {
+
+
+    private val layout = R.layout.card_view_extension_falta_judtificada // Reemplaza "nuevo_layout" con el nombre del nuevo layout que has proporcionado
+    private var clickListener: View.OnClickListener? = null
+
+    class verFaltasHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var moduloName: TextView
+        var recyclerViewRellenar: RecyclerView
+        var deployModule: ImageView
+        var cardview: CardView
+
+
+        init {
+            moduloName = view.findViewById(R.id.alumnName)
+            recyclerViewRellenar = view.findViewById(R.id.recyclerUfColorRectangle)
+            recyclerViewRellenar.layoutManager = LinearLayoutManager(view.context)
+            deployModule = view.findViewById(R.id.deployModuleColorRectangle)
+            cardview = view.findViewById(R.id.cardView1)
+        }
+    }
+
+    override fun onBindViewHolder(holder: verFaltasHolder, position: Int) {
+        val falta = faltasfecha[position]
+        holder.cardview.setOnClickListener() {
+            if (holder.recyclerViewRellenar.visibility == View.GONE) {
+                // Creamos un objeto Transition que afecte a los cambios en las vistas
+                val transition = ChangeBounds()
+
+                // Indicamos el tiempo de duración de la animación
+                transition.duration = 300
+
+                // Indicamos que se animen los cambios de manera fluida
+                transition.interpolator = AccelerateDecelerateInterpolator()
+
+                // Llamamos a la función beginDelayedTransition con nuestro ViewGroup y nuestro Transition
+                TransitionManager.beginDelayedTransition(holder.cardview as ViewGroup, transition)
+                holder.recyclerViewRellenar.visibility = View.VISIBLE
+
+                holder.deployModule.setImageResource(R.drawable.expand_less)
+            } else {
+                holder.recyclerViewRellenar.visibility = View.GONE
+                holder.deployModule.setImageResource(R.drawable.expand_more)
+            }
+        }
+        bindPackage(holder, falta)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): verFaltasHolder {
+        val view = LayoutInflater.from(parent.context).inflate(layout, parent, false)
+        view.setOnClickListener(this)
+        return verFaltasHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return faltasfecha.size
+    }
+
+    fun bindPackage(holder: verFaltasHolder, faltafecha: FaltasPorFecha) {
+
+        val adapter = UFCheckBoxAdapter(faltafecha.faltas,false) //ADAPTER INTERNO DEL CARDVIEW
+
+        holder.moduloName?.text = faltafecha.fecha.toString()
+        holder.recyclerViewRellenar?.adapter = adapter
+        holder.recyclerViewRellenar.visibility = View.GONE
+
+    }
+
+    override fun onClick(view: View?) {
+        clickListener?.onClick(view)
+    }
+
+    fun setOnClickListener(listener: View.OnClickListener) {
+        clickListener = listener
+    }
+
 }
